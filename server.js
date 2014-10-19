5859deb0feae3ddd48363673dbffdc340e341b72
Mariko Kosaka <mariko.kosaka@gmail.com>
2014-10-19 04:58:27 -0400

    port edit

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/builds',express.static(path.join(__dirname + '/builds')));
 app.use(express.static(path.join(__dirname + '/public')));
 app.use(notfound);
 
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
 console.log('listening on port 5000');
 
 /*
